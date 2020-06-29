e4a86e39f3d37164a6a766624231fe727ab9f952
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-29 15:00:05 +0800

    code clean

--- src/asker.spec.ts
+++ src/asker.spec.ts
@@ -6,6 +6,12 @@ import { asker } from './asker'
 import { normalizeConfig } from './normalize-config'
 
 test('asker()', async t => {
+  // use our normalizeConfig() helper function to get the config:
+  //
+  // const resourceName    = process.env.WECHATY_PLUGIN_QNAMAKER_RESOURCE_NAME
+  // const endpointKey     = process.env.WECHATY_PLUGIN_QNAMAKER_ENDPOINT_KEY
+  // const knowledgeBaseId = process.env.WECHATY_PLUGIN_QNAMAKER_KNOWLEDGE_BASE_ID
+
   const config = normalizeConfig({})
   const ask = asker(config)
 
