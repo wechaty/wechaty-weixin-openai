f29c72f56d7c7a97bbc2e0f4740cba8d9a24b486
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-29 14:59:08 +0800

    code clean

--- src/asker.spec.ts
+++ src/asker.spec.ts
@@ -3,21 +3,11 @@
 import test  from 'tstest'
 
 import { asker } from './asker'
+import { normalizeConfig } from './normalize-config'
 
 test('asker()', async t => {
-  const resourceName    = process.env.WECHATY_PLUGIN_QNAMAKER_RESOURCE_NAME
-  const endpointKey     = process.env.WECHATY_PLUGIN_QNAMAKER_ENDPOINT_KEY
-  const knowledgeBaseId = process.env.WECHATY_PLUGIN_QNAMAKER_KNOWLEDGE_BASE_ID
-
-  if (!resourceName || !endpointKey || !knowledgeBaseId) {
-    throw new Error('required env variables not found')
-  }
-
-  const ask = asker({
-    endpointKey,
-    knowledgeBaseId,
-    resourceName,
-  })
+  const config = normalizeConfig({})
+  const ask = asker(config)
 
   let answer = await ask('wechaty')
   t.true(answer, 'should get answer back: ' + answer)
