2567bb7fb70bbd0e79cce8b51f37431a1923775d
windmemory <wind.memory.cn@gmail.com>
2020-07-16 15:36:34 +0800

    remove openai secrets, store test secrets into github secrets

--- src/openai/index.spec.ts
+++ src/openai/index.spec.ts
@@ -5,8 +5,8 @@ import test  from 'tstest'
 import { WeixinOpenAI } from './index'
 import { SENTIMENT_MODE } from './schema/sentiment'
 
-const TEST_TOKEN = 'gtuHmKG1Qb9HxVg0CFjuwh9oyrEkR8'
-const TEST_ENCODING_AES_KEY = 'FAxvcz6hDwQWhZtryYSRqHuK7S1jhxpxfpdszOwfBW3'
+const TEST_TOKEN = process.env.WECHATY_WEIXIN_OPENAI_TOKEN!
+const TEST_ENCODING_AES_KEY = process.env.WECHATY_WEIXIN_OPENAI_ENCODING_AES_KEY!
 
 test('It should call the endpoint and get the response correctly', async t => {
   WeixinOpenAI.init(TEST_TOKEN, TEST_ENCODING_AES_KEY)
